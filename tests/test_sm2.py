0bda7c19e8abf0533b69efc741021a53066d2c60
YuboZhang <cscrypto@163.com>
2020-11-20 21:26:04 +0800

    update test data of sm2 sign/verify according to the OSCCA spec

--- tests/test_sm2.py
+++ tests/test_sm2.py
@@ -18,7 +18,12 @@ def test_sm2():
     assert data == dec_data
 
     print("-----------------test sign and verify---------------")
+    #data from OSCCA spec. SM2
+    private_key = '3945208F7B2144B13F36E38AC6D39F95889393692860B51A42FB81EF4DF7C5B8'
+    public_key = '09F9DF311E5421A150DD7D161E4BC5C672179FAD1833FC076BB08FF356F35020CCEA490CE26775A52DC6EA718CC1AA600AED05FBF35E084A6632F6072DA9AD13'
     random_hex_str = func.random_hex(sm2_crypt.para_len)
+    random_hex_str = '59276E27D506861A16680F3AD9C02DCCEF3CC1FA3CDBE4CE6D54B80DEAC1BC21'
+    data = 'F0B43E94BA45ACCAACE692ED534382EB17E6AB5A19CE7B31F4486FDFC0D28640'
     sign = sm2_crypt.sign(data, random_hex_str)
     print('sign:%s' % sign)
     verify = sm2_crypt.verify(sign, data)
@@ -45,5 +50,5 @@ def test_sm2sm3():
 
 if __name__ == '__main__':
     test_sm2()
-    test_sm2sm3()
+    #test_sm2sm3()
 
